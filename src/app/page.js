a9159e08a3ecb47f00a2159ec798a17d5ed6f4a1
kwongster <bryant.j.kwong@gmail.com>
2023-08-17 23:31:06 -0700

    add envuronment backgorund

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,29 @@
 "use client";
 import React, { useState, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import head from "../../public/adamHead.gltf";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+// import worldImage from "../../public/world.jpg";
+
+function Experience() {
+  const map = useTexture("../../public/world.jpg");
+
+  return (
+    <>
+      <ambientLight intensity={0.5} />
+      <Environment preset="sunset" />
+      <OrbitControls />
+      <mesh>
+        <sphereGeometry args={[1, 32, , 32]} />
+      </mesh>
+    </>
+  );
+}
 
 export default function Home() {
-  return <></>;
+  return (
+    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+      <color attach="background" args={["#ececec"]} />
+      <Experience />
+    </Canvas>
+  );
 }
