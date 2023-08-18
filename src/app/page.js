56144a8e75b64eaf4be481a681d4784418a4d194
kwongster <bryant.j.kwong@gmail.com>
2023-08-17 22:52:44 -0700

    test rotating cube

--- src/app/page.js
+++ src/app/page.js
@@ -2,34 +2,41 @@
 import React, { useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 
+function MyRotatingBox() {
+  const myMesh = React.useRef();
+  const [active, setActive] = useState(false);
+
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime();
+    myMesh.current.rotation.x = a;
+  });
+
+  return (
+    <mesh
+      scale={active ? 3 : 5}
+      onClick={() => setActive(!active)}
+      ref={myMesh}
+    >
+      <boxGeometry />
+      <meshPhongMaterial color="royalblue" />
+    </mesh>
+  );
+}
+
 export default function Home() {
   return (
     <>
       <div>
         <h2>HELLO WORLD</h2>
         <Canvas>
+          <MyRotatingBox />
           <ambientLight intensity={0.1} />
-          <directionalLight color="white" position={[0, 0, 5]} />
-          <mesh
-            onClick={(e) => console.log("click")}
-            onContextMenu={(e) => console.log("context menu")}
-            onDoubleClick={(e) => console.log("double click")}
-            onWheel={(e) => console.log("wheel spins")}
-            onPointerUp={(e) => console.log("up")}
-            onPointerDown={(e) => console.log("down")}
-            onPointerOver={(e) => console.log("over")}
-            onPointerOut={(e) => console.log("out")}
-            onPointerEnter={(e) => console.log("enter")}
-            onPointerLeave={(e) => console.log("leave")}
-            onPointerMove={(e) => console.log("move")}
-            onPointerMissed={() => console.log("missed")}
-            onUpdate={(self) => console.log("props have been updated")}
-          >
-            <sphereGeometry args={[1, 16, 16]} />
-            <meshStandardMaterial color="hotpink" transparent />
-          </mesh>
+          <directionalLight />
         </Canvas>
         <h2>HELLO WORLD</h2>
+        <button onClick={({ clock }) => console.log(clock.getElapsedTime())}>
+          Click me
+        </button>
       </div>
     </>
   );
