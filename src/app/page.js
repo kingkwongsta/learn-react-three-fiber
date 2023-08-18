d599bc26ed9c33a6b884f94610932a875580f72d
kwongster <bryant.j.kwong@gmail.com>
2023-08-17 22:45:24 -0700

    test event handling

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
-import { Canvas } from "@react-three/fiber";
+import React, { useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 export default function Home() {
   return (
@@ -8,10 +9,24 @@ export default function Home() {
         <h2>HELLO WORLD</h2>
         <Canvas>
           <ambientLight intensity={0.1} />
-          <directionalLight color="red" position={[0, 0, 5]} />
-          <mesh>
-            <boxGeometry args={[2, 2, 2]} />
-            <meshStandardMaterial />
+          <directionalLight color="white" position={[0, 0, 5]} />
+          <mesh
+            onClick={(e) => console.log("click")}
+            onContextMenu={(e) => console.log("context menu")}
+            onDoubleClick={(e) => console.log("double click")}
+            onWheel={(e) => console.log("wheel spins")}
+            onPointerUp={(e) => console.log("up")}
+            onPointerDown={(e) => console.log("down")}
+            onPointerOver={(e) => console.log("over")}
+            onPointerOut={(e) => console.log("out")}
+            onPointerEnter={(e) => console.log("enter")}
+            onPointerLeave={(e) => console.log("leave")}
+            onPointerMove={(e) => console.log("move")}
+            onPointerMissed={() => console.log("missed")}
+            onUpdate={(self) => console.log("props have been updated")}
+          >
+            <sphereGeometry args={[1, 16, 16]} />
+            <meshStandardMaterial color="hotpink" transparent />
           </mesh>
         </Canvas>
         <h2>HELLO WORLD</h2>
