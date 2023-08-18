3d7fd972d2197fd6abe0ab16b5aba1d0fbbdd30c
kwongster <bryant.j.kwong@gmail.com>
2023-08-17 23:13:39 -0700

    react three drei install

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,10 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-
-function MyRotatingBox() {
-  const myMesh = React.useRef();
-  const [active, setActive] = useState(false);
-
-  useFrame(({ clock }) => {
-    const a = clock.getElapsedTime();
-    myMesh.current.rotation.x = a;
-  });
-
-  return (
-    <mesh
-      scale={active ? 3 : 5}
-      onClick={() => setActive(!active)}
-      ref={myMesh}
-    >
-      <boxGeometry />
-      <meshPhongMaterial color="royalblue" />
-    </mesh>
-  );
-}
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import head from "../../public/adamHead.gltf";
 
 export default function Home() {
-  return (
-    <>
-      <div>
-        <h2>HELLO WORLD</h2>
-        <Canvas>
-          <MyRotatingBox />
-          <ambientLight intensity={0.1} />
-          <directionalLight />
-        </Canvas>
-        <h2>HELLO WORLD</h2>
-        <button onClick={({ clock }) => console.log(clock.getElapsedTime())}>
-          Click me
-        </button>
-      </div>
-    </>
-  );
+  return <></>;
 }
