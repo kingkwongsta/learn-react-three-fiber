254aed3e7248aa0f567dc3610deb841f79ef2ea2
kwongster <bryant.j.kwong@gmail.com>
2023-08-20 11:45:28 -0700

    control camera settings with drei

--- src/app/components/test.jsx
+++ src/app/components/test.jsx
@@ -1,8 +1,10 @@
 import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
 
 export default function Test() {
   return (
     <Canvas>
+      <PerspectiveCamera makeDefault fov={75} position={[0, 0, 2]} />
       <mesh>
         <boxGeometry />
         <meshStandardMaterial></meshStandardMaterial>
