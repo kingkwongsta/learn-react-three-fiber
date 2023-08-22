6dd486039977b1e0103a74579695afecc482682b
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-08-21 20:20:23 -0700

    usememo to change object and not have to rerender everything

--- src/app/components/box.jsx
+++ src/app/components/box.jsx
@@ -1,10 +1,16 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState, useMemo } from "react";
 import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 export default function Box(props) {
   const ref = useRef();
   const [hovered, setHover] = useState(false);
   const [rotate, setRotate] = useState(false);
+  const [count, setCount] = useState(0);
+  const geometry = useMemo(
+    () => [new THREE.BoxGeometry(), new THREE.SphereGeometry(0.785398)],
+    []
+  );
 
   useFrame((_, delta) => {
     if (rotate) {
@@ -16,11 +22,12 @@ export default function Box(props) {
   return (
     <>
       <mesh
-        scale={hovered ? [2, 2, 2] : [1, 1, 1]}
+        scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
         onClick={(e) => console.log(ref.current)}
-        onPointerDown={() => setRotate(!rotate)}
+        onPointerDown={() => setCount((count + 1) % 2)}
         onPointerOver={() => setHover(true)}
         onPointerOut={() => setHover(false)}
+        geometry={geometry[count]}
         ref={ref}
       >
         <boxGeometry onClick={(e) => console.log(e.object)} />
