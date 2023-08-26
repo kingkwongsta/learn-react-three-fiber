f646bb3c92b9d5d61b1ff3b4efc2d86eee6a3ca7
kwongster <bryant.j.kwong@gmail.com>
2023-08-25 18:17:26 -0700

    spotlight configured

--- src/app/page.js
+++ src/app/page.js
@@ -41,8 +41,19 @@ function Scene() {
   useHelper(directionalLight, DirectionalLightHelper, 5, "blue");
   return (
     <>
-      <Environment files="./sunset.hdr" background />
+      {/* <Environment files="./sunset.hdr" background /> */}
       <OrbitControls target={[0, 1, 0]} maxPolarAngle={[1.5]} />
+      <color args={[0x000000]} attach="background" />
+      <spotLight
+        color={0xffffff}
+        intensity={1.5}
+        position={[5, 5, 0]}
+        angle={0.6}
+        penumbra={0.5}
+        castShadow
+        ref={spotLight}
+      />
+      {/* <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} /> */}
     </>
   );
 }
@@ -52,7 +63,7 @@ export default function Home() {
 
   return (
     <div className="h-[400px]">
-      <Canvas shadows camera={{ position: [0, 0, 10], fov: 30 }}>
+      <Canvas shadows camera={{ position: [3, 2, 5], fov: 50 }}>
         <Scene />
         <axesHelper args={[10]} />
         <gridHelper args={[20, 10, 0xff0000, "grey"]} />
