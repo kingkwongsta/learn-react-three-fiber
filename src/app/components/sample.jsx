f8adb554775d09077d704db960ea0d69ff753a41
kwongster <bryant.j.kwong@gmail.com>
2023-08-27 14:44:46 -0700

    try some shorthand shortcuts

---
+++ src/app/components/sample.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
