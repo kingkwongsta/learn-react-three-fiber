f0b46dbafe0b95258b7375a05ff337bfd1987134
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-08-30 20:50:23 -0700

    10 triangles - setup vertices varibles and loop to create triangles

--- src/app/components/customgeo.jsx
+++ src/app/components/customgeo.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 export default function CustomGeo() {
+  const verticesCount = 10 * 3;
+  const positions = new Float32Array(verticesCount * 3);
+
+  for (let i = 0; i < verticesCount; i++) {
+    positions[i] = (Math.random() - 0.5) * 4;
+  }
+
   return (
     <>
       <mesh position={[1, 2, 3]}>
