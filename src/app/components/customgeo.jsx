004b4d3b4cc1c58c6f22f7a4092e33a79428964b
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-08-31 20:34:26 -0700

    modify verticie count for different shapes and randomness

--- src/app/components/customgeo.jsx
+++ src/app/components/customgeo.jsx
@@ -2,7 +2,7 @@ import { useMemo } from "react";
 import * as THREE from "three";
 
 export default function CustomGeo() {
-  const verticesCount = 10 * 3;
+  const verticesCount = 5 * 3;
   const positions = useMemo(() => {
     const positions = new Float32Array(verticesCount * 3);
     for (let i = 0; i < verticesCount * 3; i++) {
