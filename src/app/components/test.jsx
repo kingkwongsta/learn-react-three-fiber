93cf4f3bd8f593d1c26d968006a33a410e2460a5
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-09-13 20:40:34 -0700

    setup new scene with starter back code

--- src/app/components/test.jsx
+++ src/app/components/test.jsx
@@ -50,28 +50,30 @@ export default function Test() {
   //   );
   // }
 
+  useFrame((state, delta) => {
+    boxRef.current.rotation.y += delta * 0.2;
+  });
+
   return (
     <>
-      <PivotControls anchor={[1, 0, 0]}>
-        <mesh ref={sphereRef} scale={2}>
-          <boxGeometry />
-          <meshStandardMaterial color="hotpink" />
-          <Html distanceFactor={8}>
-            <h1>Test</h1>
-          </Html>
-        </mesh>
-      </PivotControls>
-      <PivotControls>
-        <Text font="./SuperMarioBros.woff" color="blue">
-          Hello World
-          <meshNormalMaterial />
-        </Text>
-        {/* <TransformControls object={sphereRef} mode="scale" />
-      <TransformControls object={sphereRef} mode="rotate" /> */}
-      </PivotControls>
-      <mesh rotation-x={-Math.PI * 0.5} position-y={-2}>
-        <planeGeometry args={[50, 50]} />
-        <MeshReflectorMaterial resolution={512} color="greenyellow" />
+      {/* <Perf position="top-left" /> */}
+
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh ref={boxRef} position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
       </mesh>
     </>
   );
