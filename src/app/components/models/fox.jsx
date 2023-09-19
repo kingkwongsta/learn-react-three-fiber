71d616561317e7b93c23f364df8e7708a485e998
kwongster <bryant.j.kwong@gmail.com>
2023-09-18 21:12:27 -0700

    scale down fox to fit floor

--- src/app/components/models/fox.jsx
+++ src/app/components/models/fox.jsx
@@ -10,7 +10,7 @@ export default function Fox(props) {
   const { nodes, materials, animations } = useGLTF("/models/Fox.gltf");
   const { actions } = useAnimations(animations, group);
   return (
-    <group ref={group} {...props} dispose={null}>
+    <group ref={group} {...props} dispose={null} scale={0.05}>
       <group>
         <group name="root">
           <skinnedMesh
